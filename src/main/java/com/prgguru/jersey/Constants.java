c0db5688af07e93cb85284161a6060000c74a3ed
Notebook Sayago <Notebook Sayago@DESKTOP-NEL8LJ9>
2016-12-24 02:11:41 -0300

    rest login online

--- src/main/java/com/prgguru/jersey/Constants.java
+++ src/main/java/com/prgguru/jersey/Constants.java
@@ -12,7 +12,11 @@ package com.prgguru.jersey;
 public class Constants {
     public static String dbClass = "com.mysql.jdbc.Driver";
     private static String dbName= "proyectox";
-    public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
-    public static String dbUser = "root";
-    public static String dbPwd = "root";
+////    public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
+//    public static String dbUser = "root";
+//    public static String dbPwd = "root";
+//    Online
+    public static String dbUrl = "jdbc:mysql://127.6.164.2:3306:3306/"+dbName;
+    public static String dbUser = "admin6t7glMh";
+    public static String dbPwd = "7vDcEQQVM_2z";
 }
